9e9269fc1a85c3962ac20f055fd8e14ac26c8b86
Marius <pit_voina@yahoo.com>
2021-02-04 21:42:40 +0200

    Added data.json Created PageHome.vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
-	root: true,
+  root: true,
 
-	env: {
-		node: true
-	},
-	extends: [
-		'plugin:vue/vue3-essential',
-		'@vue/standard'
-	],
-	parserOptions: {
-		parser: 'babel-eslint'
-	},
-	rules: {
-		'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-	}
-}
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+
+  rules: {
+    'vue/script-indent': ['error', 4, { baseIndent: 0 }],
+    'indent': 'off',
+    'no-unused-vars': 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
---
+++ src/components/PageHome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hello from page home</div>
+</template>
+
+<script>
+export default {
+    name: 'PageHome',
+};
+</script>
+
+<style scoped>
+
+</style>
